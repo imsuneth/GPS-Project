0391ece4592720ce9571cd871030b5df7327b13d
Pubudu Premathilaka <pubuduudara7@gmail.com>
2018-10-07 23:33:58 +0530

    Add files via upload

---
+++ sample python_codes/AntiClockwise_orbiting.py
@@ -0,0 +1,12 @@
+import math
+def Aclockwise_orbiting(x_coordinate,y_coordinate,time,radius,ang_velocity):
+    theta=math.atan(y_coordinate/x_coordinate)
+    new_x_coordinate=radius*math.cos(theta+ang_velocity*time)
+    new_y_coordinate=radius*math.sin(theta+ang_velocity*time)
+    return (new_x_coordinate,new_y_coordinate)
+
+'''x=0
+while(x<11):
+    print(clockwise_orbiting(1,1.7321,x,1,5))
+    x=x+0.01
+'''
---
+++ sample python_codes/Main.py
@@ -0,0 +1,3 @@
+from AntiClockwise_orbiting import Aclockwise_orbiting
+
+from Clockwise_orbiting import clockwise_orbiting
