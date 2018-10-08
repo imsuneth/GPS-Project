fb399f74ae275985863b0a964e61c5471cd23926
Suneth Samarasinghe <imsuneth@gmail.com>
2018-10-08 12:54:46 +0530

    Add files via upload

---
+++ sample python_codes/test.sh
@@ -0,0 +1,6 @@
+python Main.py testCase00Input.txt > temp.txt
+diff temp.txt testCase00Output.txt
+python Main.py testCase01Input.txt > temp.txt
+diff temp.txt testCase01Output.txt
+python Main.py testCase02Input.txt > temp.txt
+diff temp.txt testCase02Output.txt
